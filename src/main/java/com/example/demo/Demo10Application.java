323533800f797285ba022124fc9fe1fcf7f37560
Nguyen Van Tuong Duy <90355480+duyancol@users.noreply.github.com>
2023-08-06 20:22:57 +0700

    Update Demo10Application.java

--- src/main/java/com/example/demo/Demo10Application.java
+++ src/main/java/com/example/demo/Demo10Application.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import java.util.HashSet;
 
 @SpringBootApplication
+    @ComponentScan(basePackages = "com.example.demo") 
 //@EnableWebSecurity
 //@EnableJpaRepositories
 //@ComponentScan(basePackages = {"com.example.demo", "com.example.demo.config"}) // Add the package containing JwtUtil
