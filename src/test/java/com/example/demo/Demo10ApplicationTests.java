ca15fa58577f7a6163f28fae802a05bcb7893327
Nguyen Van Tuong Duy <90355480+duyancol@users.noreply.github.com>
2023-08-06 20:21:49 +0700

    Update Demo10ApplicationTests.java

--- src/test/java/com/example/demo/Demo10ApplicationTests.java
+++ src/test/java/com/example/demo/Demo10ApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+    @ComponentScan(basePackages = "com.example.demo") 
 class Demo10ApplicationTests {
 
     @Test
