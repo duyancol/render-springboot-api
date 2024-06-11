97a995b8bf22376295de83bfbf0519dd4e03587c
Nguyen Van Tuong Duy <90355480+duyancol@users.noreply.github.com>
2024-06-11 15:43:41 +0700

    Update UserRepository.java

--- src/main/java/com/example/demo/user/UserRepository.java
+++ src/main/java/com/example/demo/user/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@EnableJpaRepositories
+
 public interface UserRepository extends JpaRepository<User, Integer> {
 
   Optional<User> findByEmail(String email);
