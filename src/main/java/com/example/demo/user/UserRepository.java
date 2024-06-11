e7b9de13a0945933a22657430992cf06d074fe89
Nguyen Van Tuong Duy <90355480+duyancol@users.noreply.github.com>
2024-06-11 15:56:02 +0700

    Update UserRepository.java

--- src/main/java/com/example/demo/user/UserRepository.java
+++ src/main/java/com/example/demo/user/UserRepository.java
@@ -2,15 +2,11 @@ package com.example.demo.user;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 @Repository
-
 public interface UserRepository extends JpaRepository<User, Integer> {
 
   Optional<User> findByEmail(String email);
-  public User findByResetPasswordToken(String token);
-//  User findByEmail(String email);
-
+  User findByResetPasswordToken(String token);
 }
